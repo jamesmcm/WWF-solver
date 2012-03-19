c5175748b007a43baaee30ea307371be819902f8
Daniel O'Hanlon <dpohanlon@gmail.com>
2012-03-19 17:19:39 +0000

    Board now split into cells. TODO: Split hand, identify

--- PopBoard.py
+++ PopBoard.py
@@ -26,27 +26,25 @@ class PopBoard(object):
 
     def __init__(self):
         try:
-            self.sshot_img = Image.open("WWF1.jpg")
+            self.sshot_img = Image.open("WWF2.jpg")
         except:
             print "Could not find image file"
-        self.windowTopLeft = (0,0)
-        self.windowToBoard = (0,0)
         self.boardTLToBoardBR = (525,525)
         self.boardTLToHandTL = (127,531)
         self.handTLToHandBR = (272,44)
         self.windowToBoard = (233,60)
-        self.boardTL = (0,0)
         # Get location of the top left of the WWF window
         self.windowTopLeft = self.windowLoc(self.sshot_img)
+        self.boardTL = (self.windowTopLeft[0] + self.windowToBoard[0], self.windowTopLeft[1] + self.windowToBoard[1])
 
     def grabBoard(self, img):
         '''
         Returns (cropped) board image using translation vectors relative to reference pixel
         '''
-        boardTL = (self.windowTopLeft[0] + self.windowToBoard[0], self.windowTopLeft[1] + self.windowToBoard[1])
+        #boardTL = (self.windowTopLeft[0] + self.windowToBoard[0], self.windowTopLeft[1] + self.windowToBoard[1]) Needs to be in init (?)
         boardBR = (boardTL[0] + self.boardTLToBoardBR[0], boardTL[1] + self.boardTLToBoardBR[1])
 
-        board = img.crop(boardTL+boardBR)
+        board = img.crop(self.boardTL + boardBR)
 
         return board
 
@@ -79,9 +77,8 @@ class PopBoard(object):
                     loc = (j,i)
                     return loc
 
-    def contrastLetters(self):
+    def contrastBoard(self):
         board = self.grabBoard(self.sshot_img)
-        hand = self.grabHand(self.sshot_img)
 
         # Convert the board image into greyscale and invert
         
@@ -102,15 +99,64 @@ class PopBoard(object):
         bwBoardMDat = bwBoardM.load()
 
         # If the pixel value is not black, make it white
-        # No colours any more, I want them to turn black
+        # (No colours any more, I want them to turn black)
 
         for i in range(0, bwBoardM.size[1]):
             for j in range(0, bwBoardM.size[0]):
                 if (bwBoardMDat[j,i]!=0):
                     bwBoardMDat[j,i]=255
         # Debugging
-        bwBoardM.show()
+        #bwBoardM.show()
+        return bwBoardM
+
+    def contrastHand(self): # Merge these into one function....
+        hand = self.grabHand(self.sshot_img)
+
+        bwHand=hand.convert("L")
+        bwHandI=ImageChops.invert(bwHand)
+
+        # Multiply hand image with inverted image so that text is black
+
+        bwHandM = ImageChops.multiply(bwHand, bwHandI)
+
+        # Increase contrast
+
+        enh = ImageEnhance.Contrast(bwHandM)
+        bwHandM = enh.enhance(5.0)
+
+        # Produce pixel image object (array) for operation (operates in place)
+
+        bwHandMDat = bwHandM.load()
+
+        # If the pixel value is not black, make it white
+        # (No colours any more, I want them to turn black)
+
+        for i in range(0, bwHandM.size[1]):
+            for j in range(0, bwHandM.size[0]):
+                if (bwHandMDat[j,i]!=0):
+                    bwHandMDat[j,i]=255
+        # Debugging
+        bwHandM.show()
+        return bwHandM
+
+    def grabBoardCells(self):
+        cellSize = (35,35)
+        cellH = (0,35)
+        cellV = (35,0)
+        cell = [["0" for col in range(15)] for row in range(15)]
+
+        board = self.contrastBoard()
+
+        for i in range(0,15):
+            for j in range(0,15):
+                cellLoc= (self.boardTL[1]+i*cellH[1],self.boardTL[0]+j*cellV[0])
+                cropTo = (cellLoc[0]+cellSize[0],cellLoc[1]+cellSize[1])
+                cell[j][i]=board.crop(cellLoc+cropTo)
+                print cellLoc+cropTo
+        return cell
 
 # Debugging
 pb = PopBoard()
-pb.contrastLetters()
+#cell = pb.grabBoardCells()
+#cell[11][14].show()
+pb.contrastHand()
